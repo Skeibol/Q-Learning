2f238c81f6cb67d71c8499365335947280e6e196
Antonio Kezele <akezele35@gmail.com>
2022-10-11 22:03:23 +0200

    improved

--- qlearn.py
+++ qlearn.py
@@ -1,24 +1,10 @@
 import random
 import numpy as np
-import pygame as pg
-import os
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+import time
 
 def FormatBoard(board):
     str = np.array2string(board, precision=0, separator=' ',
                       suppress_small=True)
-    
-    
     str = str.replace("", " ", 1)
     str = str.replace("]", "")
     str = str.replace("[", "")
@@ -26,9 +12,22 @@ def FormatBoard(board):
     str = str.replace(r"1", r'P'  )
     str = str.replace(r"8",  r'X'  )
     str = str.replace(r"9",  r'O' )
-
     return str
 
+def naive_solution():
+    size = 10
+    item_start =(0,0)
+    item_dropoff = (9,9)
+    start_position = (9,0)
+    field = Field(size,item_start,item_dropoff,start_position)
+    done = False
+    steps = 0
+
+    while not done:
+        action = random.randint(0,5)
+        reward , done = field.make_action(action)
+        steps += 1
+    return steps
 
 class Field:
     def __init__(self,size,item_pickup,item_dropoff,start_position):
@@ -51,11 +50,9 @@ class Field:
             self.field[self.item_pickup[0], self.item_pickup[1]] = 8
             self.field[item_dropoff[0],item_dropoff[1]] = 9
         board_formatted = FormatBoard(self.field)
+        print("---"*21)
         print(board_formatted)
-        pg.time.wait(150)
-        print("\033[F"*21)
-
-
+        time.sleep(0.25)
 
     def get_number_of_states(self):
         return self.size*self.size*self.size*self.size*2
@@ -114,50 +111,40 @@ class Field:
             else:
                 return 20,True  
 
-field = Field(10, (0,0), (9,9), (9,0))
-
-def naive_solution():
-    size = 10
-    item_start =(0,0)
-    item_dropoff = (9,9)
-    start_position = (9,0)
-    field = Field(size,item_start,item_dropoff,start_position)
-    done = False
-    steps = 0
-
-    while not done:
-        action = random.randint(0,5)
-        reward , done = field.make_action(action)
-        steps += 1
-       
-    return steps
 
 size = 10
 item_start =(0,0)
 item_dropoff = (9,9)
 start_position = (9,0)
 
-
+field = Field(size,item_start,item_dropoff,start_position)
 number_of_states = field.get_number_of_states()
 number_of_actions = 6
 q_table = np.zeros((number_of_states,number_of_actions))
+
+
 epsilon = 0.1
 alpha = 0.01
 gamma = 0.6
-c_list =[]
+
+
 minSteps = 50
-for _ in range(100000):
-    steps = 0
+maxReward = 0
+for epoch in range(100000):
     field = Field(size,item_start,item_dropoff,start_position)
+
     done = False
+    drawing = False
     rewardS = 0
+    steps = 0
+
     while not done:
-        if _ != 0 and _ % 10000 == 0:
+        if epoch != 0 and epoch % 2000 == 0:
             field.draw()
-            
+            drawing = True
 
         state = field.get_state()
-        if random.uniform(0,1)<epsilon:
+        if random.uniform(0,1)<epsilon and not drawing:
             action = random.randint(0,5)
         else:
             action = np.argmax(q_table[state])
@@ -167,13 +154,18 @@ for _ in range(100000):
         new_state = field.get_state()
         new_state_max = np.max(q_table[new_state])
         q_table[state,action] = (1-alpha)*q_table[state,action] + alpha*(reward + gamma*new_state_max - q_table[state,action])
+
         steps+=1
+        rewardS+=reward
+
     if steps<minSteps:
         minSteps = steps
+        maxReward = rewardS
 
-    print(steps)
+    if epoch != 0 and epoch % 2000 == 0:
+        print(f"Min steps: {minSteps}, Max reward: {maxReward}, Epoch: {epoch}")
 
-print(minSteps)
+print(minSteps,maxReward)
     
 
 
